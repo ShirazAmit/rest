390f41a6878ae8fdd01819a042aae67ea8cd5e71
ShirazAmit <shirazam054@gmail.com>
2022-07-14 21:22:20 +0300

    Register user 2

--- mongoDB/models/meals.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-const mealSchema = new mongoose.Schema({
-    author: String,
-    text: String,
-    color: { type: String, default: 'black' },
-    image: Buffer,
-});
-
-module.exports = mongoose.model('Meal', mealSchema);
